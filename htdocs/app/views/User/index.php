f48c6379083113a896e7699e6987fb6c7c887558
Shooter-06 <john190curry@gmail.com>
2022-11-16 11:10:14 -0500

    Views updated

--- htdocs/app/views/User/index.php
+++ htdocs/app/views/User/index.php
@@ -1,130 +1,76 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-	
-	<head>
-
-		<!-- Jquery -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-		<!-- Bootstrap CSS --> 
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/
-		bootstrap.min.css" integrity="sha384-Vkoo8×4CGsO3+Hhxv8T/
-		Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-		<!-- Bootstrap JS -->
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-		integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-		crossorigin="anonymous"></script>
-		<title>Log in Page</title>
-
-		<style>
-
-			body{
-					background-color:#FDF5E6;
-				}
-				
-    		h1{
-    			font-size: 30px;
-    			font-family: "Times New Roman", Times, serif;
-			    position: absolute;
-			    text-align: center;
-			    top: 30%;
-    			width: 100%;
-    		}
-
-    		.form-group1{
-    			font-family: "Times New Roman", Times, serif;
-			    position: absolute;
-			    text-align: center;
-			    top: 40%;
-    			width: 100%;
-    		}
-
-    		#username{
-    			font-family: "Times New Roman", Times, serif;
-			    position: absolute;
-			    margin-top: 10px;
-			    margin-left: 36%;
-    			width: 400px;
-    		}
-
-    		.form-group2{
-    			font-family: "Times New Roman", Times, serif;
-			    position: absolute;
-			    text-align: center;
-			    top: 55%;
-    			width: 100%;
-    		}
-
-    		#password{
-    			font-family: "Times New Roman", Times, serif;
-			    position: absolute;
-			    margin-top: 10px;
-			    margin-left: 36%;
-    			width: 400px;
-    		}
-
-    		button{
-    			font-family: "Times New Roman", Times, serif;
-			    position: absolute;
-			    text-align: center;
-			    margin-top: 480px;
-			    margin-left: 40%;
-    			width: 300px;
-    		}
-
-    		p{
-    			font-family: "Times New Roman", Times, serif;
-			    position: absolute;
-			    text-align: center;
-			    margin-top: 510px;
-			    margin-left: 40%;
-    			width: 300px;
-    		}
-
-    		a:hover{
-    			text-decoration: none;
-    		}
-
-		</style>
-	</head>
-	
-	<body>
-		<?php
-			if(isset($_GET['error'])){ ?>
-				<div class="alert alert-danger alert-dismissible">
-  					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-  					<?= $_GET['error'] ?>
-				</div>
-		<?php  }
-			if(isset($_GET['message'])){ ?>
-				<div class="alert alert-success alert-dismissible">
-  					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-  					<?= $_GET['message'] ?>
-				</div>
-		<?php  }
-		?>
-
-		<div>
-			<h1>Welcome to Blah Blah</h1>
-		</div>
-
-		<form action='' method='post'>
-			<div class="form-group1">
-    			<label for="username">Username</label>
-    			<input type="text" class="form-control" id="username" name='username' placeholder="Enter username">
-  			</div><br>
-
-  			<div class="form-group2">
-    			<label for="password">Password</label>
-    			<input type="password" class="form-control" id="password" name="password" placeholder="Enter Password">
-  			</div><br>
-  			<br>
-  			<button type="submit" name='action' value='Login' class="btn btn-primary">Login</button>
-  			<br>
-		</form>
-
-		<p>Don't have an Account? <a href ="/User/register">Sign Up</a></p>
-	</body>
-
+  <head>
+    <meta charset="utf-8" />
+    <!--<meta name=description content="This site was generated with Anima. www.animaapp.com"/>-->
+    <!-- <link rel="shortcut icon" type=image/png href="https://animaproject.s3.amazonaws.com/home/favicon.png" /> -->
+    <meta name="viewport" content="width=1788, maximum-scale=1.0" />
+    <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />
+    <meta name="og:type" content="website" />
+    <meta name="twitter:card" content="photo" />
+    <link rel="stylesheet" type="text/css" href="app/views/user/LoginCssFiles/css/frame-525.css" />
+    <link rel="stylesheet" type="text/css" href="app/views/user/LoginCssFiles/css/styleguide.css" />
+    <link rel="stylesheet" type="text/css" href="app/views/user/LoginCssFiles/css/globals.css" />
+  </head>
+  <body style="margin: 0; background: #ffffff">
+    <input type="hidden" id="anPageName" name="page" value="frame-525" />
+    <div class="container-center-horizontal">
+      <div class="frame-525 screen">
+        <img class="image" src="app/views/user/LoginCssFiles/img/image-1@2x.png" alt="Image" />
+        <div class="flex-col">
+          <div class="flex-row">
+            <img class="logo" src="app/views/user/LoginCssFiles/img/logo-1@2x.png" alt="Logo" />
+            <div class="flex-col-1">
+              <h1 class="title valign-text-middle">
+                <span><span>Inventory</span> </span>
+              </h1>
+              <div class="title-container">
+                <img class="title-line" src="app/views/user/LoginCssFiles/img/title-line-1@2x.png" alt="Title line" />
+                <div class="subtitle valign-text-middle">
+                  <span><span>Healing Place</span> </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="login-info">
+            <div class="overlap-group">
+              <div class="employee-button">
+                <div class="employee valign-text-middle">
+                  <span><span>Employee</span> </span>
+                </div>
+              </div>
+              <div class="admin-button">
+                <div class="admin valign-text-middle">
+                  <span><span>Admin</span> </span>
+                </div>
+              </div>
+              <img class="line-2" src="app/views/user/LoginCssFiles/img/line-2-1@2x.png" alt="Line 2" />
+            </div>
+            <div class="employee-container">
+              <div class="employee-username valign-text-middle roundedmplus1c-regular-normal-black-16px">
+                <span><span class="roundedmplus1c-regular-normal-black-16px">Employee Username</span> </span>
+              </div>
+              <div class="login-employee-name-text"></div>
+            </div>
+            <img class="d-line" src="app/views/user/LoginCssFiles/img/passwordline-1@2x.png" alt="EmployeeIdLine" />
+            <div class="password-container">
+              <div class="password valign-text-middle roundedmplus1c-regular-normal-black-16px">
+                <span><span class="roundedmplus1c-regular-normal-black-16px">Password</span> </span>
+              </div>
+              <div class="login-employee-password-text"></div>
+            </div>
+            <img class="d-line" src="app/views/user/LoginCssFiles/img/passwordline-1@2x.png" alt="PasswordLine" />
+            <div class="login-button">
+              <div class="log-in valign-text-middle">
+                <span><span>Log in</span> </span>
+              </div>
+              <!-- <div class="arrow-right valign-text-middle">
+                <span><span>arrow-right</span> </span>
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
