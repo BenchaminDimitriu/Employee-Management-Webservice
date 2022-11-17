2538cdfa7041837bb9dcaf53f3e9b8d3c4871a05
Shooter-06 <john190curry.com>
2022-11-17 13:05:04 -0500

    error

---
+++ htdocs/app/views/Error/error.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <!--<meta name=description content="This site was generated with Anima. www.animaapp.com"/>-->
+    <!-- <link rel="shortcut icon" type=image/png href="https://animaproject.s3.amazonaws.com/home/favicon.png" /> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="shortcut icon" type="app/views/Error/image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />
+    <meta name="og:type" content="website" />
+    <meta name="twitter:card" content="photo" />
+    <link rel="stylesheet" type="text/css" href="app/views/Error/css/frame-528.css" />
+    <link rel="stylesheet" type="text/css" href="app/views/Error/css/styleguide.css" />
+    <link rel="stylesheet" type="text/css" href="app/views/Error/css/globals.css" />
+  </head>
+  <body style="margin: 0; background: #ffffff">
+    <input type="hidden" id="anPageName" name="page" value="frame-528" />
+    <div class="container-center-horizontal">
+      <div class="frame-528 screen">
+        <div class="login-error-popup">
+          <div class="overlap-group">
+            <div class="rectangle-5"></div>
+            <div class="error valign-text-middle">
+              <span><span>ERROR</span> </span>
+            </div>
+            <div class="input-username-password valign-text-middle">
+              <span><span>Input Username &amp; Password</span> </span>
+            </div>
+          </div>
+          <div class="save-button">
+            <div class="cancel valign-text-middle">
+              <span><span>Cancel</span> </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
