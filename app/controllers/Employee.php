405edde9c496ed354bf08139c6f327e565bdc776
JulienBernardo2 <4gums@bell.net>
2022-11-21 09:11:10 -0500

    emploiyee controller

---
+++ app/controllers/Employee.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\controllers;
+
+class User extends \app\core\Controller{
+
+	public function index(){
+		$employee = new \app\models\Employee();
+		$employees = $employee->getAll();
+
+		$this->view('Employee/index', $employees);
+	}
+}
+?>
--- app/views/Item/indexAdmin.php
+++ app/views/Item/indexAdmin.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="/css/Item/items.css" />
+    <link rel="stylesheet" type="text/css" href="/css/Item/item.css" />
     <link rel="stylesheet" type="text/css" href="/css/Item/styleguide.css" />
     <link rel="stylesheet" type="text/css" href="/css/Item/global.css" />
   </head>
@@ -15,59 +15,49 @@
   <body>
     <div class="item-page-admin">
       <div class="overlap-group9">
-        <div class="itemInventory">
-          
-          <div class="itemIcon valign-text-middle">
-            <i class="fa-solid fa-boxes-stacked"></i>
-          </div>  
+        
+        <div class="itemInventory">  
           <div class="itemText valign-text-middle roundedmplus1c-regular-normal-white-16px">
+            <i class="fa-solid fa-boxes-stacked"></i>
             <span>Items</span>
           </div>
         </div>
 
         <div class="category">
-          <div class="categoryIcon valign-text-middle">
-            <span><i class="fa-solid fa-bookmark"></i></span>
-          </div>
           <div class="categoryText valign-text-middle roundedmplus1c-regular-normal-white-16px">
+            <span><i class="fa-solid fa-bookmark"></i></span>
             <span>Category</span>
           </div>
         </div>
 
         <div class="employees">
-          <div class="employeeIcon valign-text-middle">
-            <span><i class="fa-solid fa-users"></i></span>
-          </div>
-          <div class="employeeText valign-text-middle roundedmplus1c-regular-normal-white-16px">
-            <span>Employees</span>
-          </div>
+          <a  href ="/Employee/index">
+            <div class="employeeText valign-text-middle roundedmplus1c-regular-normal-white-16px">
+              <span><i class="fa-solid fa-users"></i></span>
+              <span>Employees</span>
+            </div>
+          </a>
         </div>
 
         <img class="logo" src="/images/Item/logo-1@2x.png" alt="Logo" />
 
         <div class="profile">
-          <div class="profileIcon valign-text-middle">
-            <span><i class="fa fa-user"></i></span>
-          </div>
           <div class="profileText valign-text-middle roundedmplus1c-regular-normal-white-16px">
+            <span><i class="fa fa-user"></i></span>
             <span>Profile</span>
           </div>
         </div>
 
         <div class="notification">
-          <div class="notifIcon valign-text-middle">
-            <span><i class="fa fa-bell"></i></span>
-          </div>
           <div class="notifText valign-text-middle roundedmplus1c-regular-normal-white-16px">
+            <span><i class="fa fa-bell"></i></span>
             <span>Message</span>
           </div>
         </div>
 
         <div class="logout">
-          <div class="logoutIcon valign-text-middle">
-            <span><i class="fa fa-power-off"></i></span>
-          </div>
           <div class="logoutText valign-text-middle roundedmplus1c-regular-normal-white-16px">
+            <span><i class="fa fa-power-off"></i></span>
             <span>Logout</span>
           </div>
         </div>
